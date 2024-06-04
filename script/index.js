613a9fa5c6dcb43ef20e6e0769cab496ea1819c9
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-04 14:35:54 +0200

    min2sec

--- script/index.js
+++ script/index.js
@@ -2,8 +2,8 @@ let input = document.querySelector('input');
 let button = document.querySelector('button');
 let span = document.querySelector('span')
 
-function minToSec(min){
-    return  min * 60;
+function minToSec(min){ //function for converting minutes to seconds
+    return  min * 60; 
 }
 
 button.addEventListener('click', () =>{
